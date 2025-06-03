45abe42d5f01ef461293b7725b7353fc4a852533
Alberto Carlos <albertocarlos.carbonell@weecover.com>
2025-06-02 21:41:40 -0300

    fix: other problem with css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss';
+import animatePlugin from 'tw-animate-css';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animatePlugin],
 };
 
 export default config;
